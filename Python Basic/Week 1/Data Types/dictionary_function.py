c412c7d839b961c46933e9da085695863df0f59d
Abdullah Al Noman <41755275+nomancseku@users.noreply.github.com>
2020-10-24 23:37:39 +0600

    --

--- Python Basic/Week 1/Data Types/dictionary_function.py
+++ Python Basic/Week 1/Data Types/dictionary_function.py
@@ -8,3 +8,20 @@ my_nums = {1 : 1, 2 : 4, 3 : 9, 4 : 16}
 my_nums[5] = 25
 
 print(my_nums)
+
+
+#finding the key
+nums = {1: "one", 2: "two", 3: "three"}
+
+print(1 in nums)
+print("three" in nums)
+print(4 not in nums)
+
+
+# get() function
+my_nums = {1 : 1, 2 : 4, 3 : 9, 4 : 16}
+print(my_nums.get(5))
+
+# default value with get()
+my_nums = {1 : 1, 2 : 4, 3 : 9, 4 : 16}
+print(my_nums.get(5, "5 not in my numbers!"))
