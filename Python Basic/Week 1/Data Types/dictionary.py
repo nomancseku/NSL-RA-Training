e1316ce0a9e923d4d1ac8675ae2b2beff19b9484
Abdullah Al Noman <41755275+nomancseku@users.noreply.github.com>
2020-10-24 23:33:45 +0600

    --

---
+++ Python Basic/Week 1/Data Types/dictionary.py
@@ -0,0 +1,9 @@
+my_marks = {"Bengali": 80, "English": 85, "Math": 90}
+print(my_marks["Math"])
+
+my_marks = {"Bengali" : [30, 35, 32], "English" : [45, 52, 33], "Math": [60, 74, 58]}
+print(my_marks["Math"])
+
+#funny
+my_marks = {True : "Bengali"}
+print(my_marks[True])
---
+++ Python Basic/Week 1/Data Types/dictionary_function.py
@@ -0,0 +1,10 @@
+my_nums = {1 : 1, 2 : 4, 3 : 9, 4 : "What?"}
+my_nums[4] = 16
+
+print(my_nums)
+
+#adding new
+my_nums = {1 : 1, 2 : 4, 3 : 9, 4 : 16}
+my_nums[5] = 25
+
+print(my_nums)
