4ca937b490c24903f29a9324212552f488e3beba
Abdullah Al Noman <41755275+nomancseku@users.noreply.github.com>
2020-11-25 13:19:47 +0600

    25 Nov
    Really sorry for late.

---
+++ Python Basic/Week 2/OOP/properties.py
@@ -0,0 +1,22 @@
+class Pizza:
+    def __init__(self, toppings):
+        self.toppings = toppings
+        self._pineapple_allowed = False
+
+    @property
+    def pineapple_allowed(self):
+        return self._pineapple_allowed
+
+    @pineapple_allowed.setter
+    def pineapple_allowed(self, value):
+    	if value:
+    		password = input("Enter password: ")
+    		if password == "noman":
+    			self._pineapple_allowed = value
+    	else:
+    		raise ValueError("Aler! Intruder!")
+
+pizza = Pizza(["cheese", "tomato"])
+print(pizza.pineapple_allowed)
+pizza.pineapple_allowed = True
+print(pizza.pineapple_allowed)
