ac7b3880256da247d0f6faab3329327ff68b32c6
Abdullah Al Noman <41755275+nomancseku@users.noreply.github.com>
2021-01-13 18:55:59 +0600

    pygame tutorial

---
+++ Data Structure Algorithm/Week 3/Breakout Game.py
@@ -0,0 +1,73 @@
+#Import the pygame library and initialise the game engine
+import pygame
+#Let's import the Paddle Class
+from paddle import Paddle
+
+pygame.init()
+
+# Define some colors
+WHITE = (255,255,255)
+DARKBLUE = (36,90,190)
+LIGHTBLUE = (0,176,240)
+RED = (255,0,0)
+ORANGE = (255,100,0)
+YELLOW = (255,255,0)
+
+score = 0
+lives = 3
+
+# Open a new window
+size = (800, 600)
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Breakout Game")
+
+#This will be a list that will contain all the sprites we intend to use in our game.
+all_sprites_list = pygame.sprite.Group()
+
+#Create the Paddle
+paddle = Paddle(LIGHTBLUE, 100, 10)
+paddle.rect.x = 350
+paddle.rect.y = 560
+
+# Add the paddle to the list of sprites
+all_sprites_list.add(paddle)
+
+# The loop will carry on until the user exit the game (e.g. clicks the close button).
+carryOn = True
+
+# The clock will be used to control how fast the screen updates
+clock = pygame.time.Clock()
+
+# -------- Main Program Loop -----------
+while carryOn:
+    # --- Main event loop
+    for event in pygame.event.get(): # User did something
+        if event.type == pygame.QUIT: # If user clicked close
+              carryOn = False # Flag that we are done so we exit this loop
+
+    # --- Game logic should go here
+    all_sprites_list.update()
+
+    # --- Drawing code should go here
+    # First, clear the screen to dark blue.
+    screen.fill(DARKBLUE)
+    pygame.draw.line(screen, WHITE, [0, 38], [800, 38], 2)
+
+    #Display the score and the number of lives at the top of the screen
+    font = pygame.font.Font(None, 34)
+    text = font.render("Score: " + str(score), 1, WHITE)
+    screen.blit(text, (20,10))
+    text = font.render("Lives: " + str(lives), 1, WHITE)
+    screen.blit(text, (650,10))
+
+    #Now let's draw all the sprites in one go. (For now we only have 2 sprites!)
+    all_sprites_list.draw(screen)
+
+    # --- Go ahead and update the screen with what we've drawn.
+    pygame.display.flip()
+
+    # --- Limit to 60 frames per second
+    clock.tick(60)
+
+#Once we have exited the main program loop we can stop the game engine:
+pygame.quit()
