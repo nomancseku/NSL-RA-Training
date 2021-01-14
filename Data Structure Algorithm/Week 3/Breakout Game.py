399abae199546a337c142690290bfa86ba6b26d9
Abdullah Al Noman <41755275+nomancseku@users.noreply.github.com>
2021-01-14 22:58:10 +0600

    Update Breakout Game.py

--- Data Structure Algorithm/Week 3/Breakout Game.py
+++ Data Structure Algorithm/Week 3/Breakout Game.py
@@ -3,6 +3,7 @@ import pygame
 #Let's import the Paddle Class
 from paddle import Paddle
 from ball import Ball
+from brick import Brick
 
 pygame.init()
 
@@ -34,6 +35,26 @@ paddle.rect.y = 560
 ball = Ball(WHITE,10,10)
 ball.rect.x = 345
 ball.rect.y = 195
+
+all_bricks = pygame.sprite.Group()
+for i in range(7):
+    brick = Brick(RED,80,30)
+    brick.rect.x = 60 + i* 100
+    brick.rect.y = 60
+    all_sprites_list.add(brick)
+    all_bricks.add(brick)
+for i in range(7):
+    brick = Brick(ORANGE,80,30)
+    brick.rect.x = 60 + i* 100
+    brick.rect.y = 100
+    all_sprites_list.add(brick)
+    all_bricks.add(brick)
+for i in range(7):
+    brick = Brick(YELLOW,80,30)
+    brick.rect.x = 60 + i* 100
+    brick.rect.y = 140
+    all_sprites_list.add(brick)
+    all_bricks.add(brick)
 
 # Add the paddle to the list of sprites
 all_sprites_list.add(paddle)
@@ -69,6 +90,18 @@ while carryOn:
         ball.velocity[0] = -ball.velocity[0]
     if ball.rect.y>590:
         ball.velocity[1] = -ball.velocity[1]
+        lives -= 1
+        if lives == 0:
+            #Display Game Over Message for 3 seconds
+            font = pygame.font.Font(None, 74)
+            text = font.render("GAME OVER", 1, WHITE)
+            screen.blit(text, (250,300))
+            pygame.display.flip()
+            pygame.time.wait(3000)
+ 
+            #Stop the Game
+            carryOn=False
+
     if ball.rect.y<40:
         ball.velocity[1] = -ball.velocity[1]
 
@@ -77,7 +110,24 @@ while carryOn:
       ball.rect.x -= ball.velocity[0]
       ball.rect.y -= ball.velocity[1]
       ball.bounce()
+
+    #Check if there is a brick and ball collision
+    brick_collision_list = pygame.sprite.spritecollide(ball,all_bricks,False)
+    for brick in brick_collision_list:
+      ball.bounce()
+      score += 1
+      brick.kill()
+      if len(all_bricks)==0:
+           #Display Level Complete Message for 3 seconds
+            font = pygame.font.Font(None, 74)
+            text = font.render("LEVEL COMPLETE", 1, WHITE)
+            screen.blit(text, (200,300))
+            pygame.display.flip()
+            pygame.time.wait(3000)
  
+            #Stop the Game
+            carryOn=False
+
     # --- Drawing code should go here
     # First, clear the screen to dark blue.
     screen.fill(DARKBLUE)
