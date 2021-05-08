c34f4bdc2e0d959b34f2bacff4a5637fc2343002
Abdullah Al Noman <41755275+nomancseku@users.noreply.github.com>
2021-05-08 23:56:38 +0600

    speacial variable in python
    __name__

---
+++ Python Basic/Week 2/10. Speacial Variables/first.py
@@ -0,0 +1,4 @@
+if __name__ == '__main__':
+	print('first script is executing as main script!')
+else:
+	print('first script is imported from another script')
---
+++ Python Basic/Week 2/10. Speacial Variables/second.py
@@ -0,0 +1,6 @@
+import first
+
+if __name__ == '__main__':
+	print('second script is executing as main script!')
+else:
+	print('second script is imported from another script')
